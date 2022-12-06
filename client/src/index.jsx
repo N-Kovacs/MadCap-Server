006b4acde79ba2433646033cec430f5ff538046f
colespen <spencerlewiscole@gmail.com>
2022-12-05 20:37:22 -0500

    finish clock and most color scheme

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,6 +11,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/900.css';
+import './fonts/jones-outline.ttf';
 import './index.scss';
 
 import App from './components/App';
