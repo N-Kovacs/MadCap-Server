2a6243f9768b80e82425294dc39cdf1d404a08a2
colespen <spencerlewiscole@gmail.com>
2022-12-07 09:33:35 -0500

    fix avatar highlight

--- client/src/components/Lobby/PlayersList.jsx
+++ client/src/components/Lobby/PlayersList.jsx
@@ -31,7 +31,7 @@ export default function PlayersList(props) {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'flex-start',
-          bgcolor: (player.id === props.currentUser) && '#e9f8fe',
+          bgcolor: (player.id === props.currentUser) && '#e9f0fe',
           width: '100%', pt: '10px', mb: '8px', pb: '0px', borderTopLeftRadius: '26px'
 
         }}>
@@ -89,7 +89,7 @@ export default function PlayersList(props) {
                   flexDirection: 'column',
                   alignItems: 'flex-start',
                   bgcolor:
-                    (host && host.id === props.currentUser) && '#e9f8fe',
+                    (host && host.id === props.currentUser) && '#e9f0fe',
                   width: '100%', pt: '10px', pb: '0px', borderTopLeftRadius: '26px'
                 }}>
                   <ListItemAvatar sx={{ display: 'flex', alignItems: 'center', justifyItems: 'space-around' }}>
